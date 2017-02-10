8ee38728537508b6fd826890be3ad063374e5cbc
Your Name <you@example.com>
2017-02-10 20:24:00 +0530

    Added ML encoding, cleanup + documentation

--- beta_trainScriptClass_conditional.py
+++ beta_trainScriptClass_conditional.py
@@ -43,43 +43,38 @@ for i in range(tr_iters):
         print('Iteration %.4d  Train Loss: %6.3f'%(i+1, loss_val))
 
     if (i+1) % 100000 == 0:
-        rand_sample = mnist.validation.next_batch(params['batch_size'])[0]
-        generated = VAE.generateSample_conditional(sess,rand_sample)
-        os.system('mkdir -p generated_class_conditional/iter_%.6d'%(i+1))
+		# Generate some samples from the current model and store
+        generated = VAE.generateSample(sess,params['batch_size'])
+        os.system('mkdir -p generated_class/iter_%.6d'%(i+1))
         for im in range(params['batch_size']):
             reshaped_image = generated[im]
             reshaped_image = reshaped_image.reshape(28, 28)
             scipy.misc.toimage(reshaped_image, cmin=0.0, cmax=1.0).save('generated_class/iter_%.6d/img%.3d.png'%(i+1,im))
-        #for im in range(params['batch_size']):
-            #original_image = rand_sample[im].reshape(28,28)
-            #reshaped_image = generated[im]
-            #reshaped_image = reshaped_image.reshape(28, 28)
-            #scipy.misc.toimage(np.concatenate((original_image,reshaped_image),axis=1), cmin=0.0, cmax=1.0).save('generated_class_conditional/iter_%.6d/img%.3d.png'%(i+1,im))
-
-        save_path = saver.save(sess, 'generated_class_conditional/iter_%.6d/checkpoint.ckpt'%(i+1))
+        save_path = saver.save(sess, 'generated_class/iter_%.6d/checkpoint.ckpt'%(i+1))
         print('Saved model to %s'%(save_path))
 
 
 # Examining latent variables
 n_test_imgs = 100
+# Sample some random images from the validtion set
 rand_sample, rand_labels = mnist.validation.next_batch(n_test_imgs)
 rand_labels = np.array([np.argmax(x) for x in rand_labels])
-print rand_labels
-encoded = VAE.encode(sess,rand_sample)
+# Get a z for each image and then decode it back to an image
+encoded = VAE.encode_ML(sess,rand_sample)
 decoded = VAE.decode(sess,encoded)
+#decoded = VAE.generateSampleConditional_ML(sess,rand_sample)
 os.system('mkdir -p generated_class_conditional/latent')
 latent_max = np.amax(np.abs(np.vstack(encoded)), axis=0)
+# Organize generated images by label
 for digit in range(10) :
 	imlist = [np.zeros((1,28+28+7*params['z_size']))]
 	for idx in np.where(rand_labels==digit)[0] :
-		print idx, digit
 		reshaped_image = rand_sample[idx].reshape(28, 28)
 		decoded_image = decoded[idx].reshape(28,28)
+		# Divide each latent feature by the maximum observed among the
+		# encoded versions of the sample images and take abs to
+		# normalize it to [0,1] range to visualize as a grayscale colour
 		latent_vis = [ x*np.ones((28,7)) for x in abs(encoded[idx])/latent_max]
 		imlist.append(np.concatenate([reshaped_image,decoded_image]+latent_vis,axis=1))
-		print len(imlist)
 	scipy.misc.toimage(np.concatenate(imlist,axis=0), cmin=0.0, cmax=1.0).save('generated_class_conditional/latent/%d.png'%(digit))
-		
-#for im in range(n_test_imgs):
-	#print encoded[im], np.argmax(rand_labels[im])
 
