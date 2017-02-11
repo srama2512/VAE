73ed949320e3a60be0fdca4e5cebc77654805485
Santhosh <santhoshkumar.25dec@gmail.com>
2017-02-11 11:50:13 +0530

    added relu for better training

--- betavae_cnn.py
+++ betavae_cnn.py
@@ -51,14 +51,14 @@ class vae(object):
         # Convolution layer 1
         self.conv_w_1 = getWeight([4, 4, 1, 64])
         self.conv_b_1 = getBias([64])
-        conv_1 = tf.nn.conv2d(self.X_placeholder, self.conv_w_1, \
-                     strides=[1, 2, 2, 1], padding='VALID') + self.conv_b_1
+        conv_1 = tf.nn.relu(tf.nn.conv2d(self.X_placeholder, self.conv_w_1, \
+                     strides=[1, 2, 2, 1], padding='VALID') + self.conv_b_1)
         
         # Convolution layer 2
         self.conv_w_2 = getWeight([3, 3, 64, 32])
         self.conv_b_2 = getBias([32])
-        conv_2 = tf.nn.conv2d(conv_1, self.conv_w_2, \
-                     strides=[1, 2, 2, 1], padding='VALID')+self.conv_b_2
+        conv_2 = tf.nn.relu(tf.nn.conv2d(conv_1, self.conv_w_2, \
+                     strides=[1, 2, 2, 1], padding='VALID')+self.conv_b_2)
         
         # Projection layer
         conv_2_reshaped = tf.reshape(conv_2, [-1, self.fc_size])
@@ -66,11 +66,11 @@ class vae(object):
         # Get mu(X) and log_Sigma_diag(X)
         self.mu_weight = getWeight([self.fc_size, self.z_size])
         self.mu_bias = getBias([self.z_size])
-        self.mu_X = tf.tanh(tf.matmul(conv_2_reshaped, self.mu_weight) + self.mu_bias)
+        self.mu_X = tf.matmul(conv_2_reshaped, self.mu_weight) + self.mu_bias
 
         self.Sigma_weight = getWeight([self.fc_size, self.z_size])
         self.Sigma_bias = getBias([self.z_size])
-        self.log_Sigma_X_diag = tf.tanh(tf.matmul(conv_2_reshaped, self.Sigma_weight) + self.Sigma_bias)
+        self.log_Sigma_X_diag = tf.matmul(conv_2_reshaped, self.Sigma_weight) + self.Sigma_bias
 
     def getLatentSampler(self):
 
@@ -78,27 +78,27 @@ class vae(object):
         self.z_sample = tf.mul(tf.sqrt(tf.exp(self.log_Sigma_X_diag)), self.eps) + self.mu_X
 
     def getGenerator(self):
-		# Up-projection layer
-		self.gen_proj_w = getWeight([self.z_size, self.fc_size])
-		self.gen_proj_b = getBias([self.fc_size])
-		gen_proj = tf.matmul(self.z_sample, self.gen_proj_w) + self.gen_proj_b  
-		proj_reshaped = tf.reshape(gen_proj, [self.batch_size, 6, 6, 32])
-		# Transposed Convolution layer 1
-		# Note: conv2d_transpose expects the filter size to be specified
-		# as (height, width, OUT_channels, IN_channels)
-		self.trans_conv_w_1 = getWeight([3, 3, 64, 32])
-		self.trans_conv_b_1 = getBias([64])
-		trans_conv_1 = tf.nn.conv2d_transpose(proj_reshaped, self.trans_conv_w_1, output_shape=[self.batch_size, 13, 13, 64], strides=[1, 2, 2, 1], padding='VALID') + self.trans_conv_b_1
+        # Up-projection layer
+        self.gen_proj_w = getWeight([self.z_size, self.fc_size])
+        self.gen_proj_b = getBias([self.fc_size])
+        gen_proj = tf.matmul(self.z_sample, self.gen_proj_w) + self.gen_proj_b  
+        proj_reshaped = tf.nn.relu(tf.reshape(gen_proj, [self.batch_size, 6, 6, 32]))
+        # Transposed Convolution layer 1
+        # Note: conv2d_transpose expects the filter size to be specified
+        # as (height, width, OUT_channels, IN_channels)
+        self.trans_conv_w_1 = getWeight([3, 3, 64, 32])
+        self.trans_conv_b_1 = getBias([64])
+        trans_conv_1 = tf.nn.relu(tf.nn.conv2d_transpose(proj_reshaped, self.trans_conv_w_1, output_shape=[self.batch_size, 13, 13, 64], strides=[1, 2, 2, 1], padding='VALID') + self.trans_conv_b_1)
 
-		# Transposed Convolution layer 2
-		# Note: conv2d_transpose expects the filter size to be specified
-		# as (height, width, OUT_channels, IN_channels)
-		self.trans_conv_w_2 = getWeight([4, 4, 1, 64])
-		self.trans_conv_b_2 = getBias([1])
-		trans_conv_2 = tf.nn.conv2d_transpose(trans_conv_1, self.trans_conv_w_2, output_shape=[self.batch_size, 28, 28, 1], strides=[1, 2, 2, 1], padding='VALID')+self.trans_conv_b_2
+        # Transposed Convolution layer 2
+        # Note: conv2d_transpose expects the filter size to be specified
+        # as (height, width, OUT_channels, IN_channels)
+        self.trans_conv_w_2 = getWeight([4, 4, 1, 64])
+        self.trans_conv_b_2 = getBias([1])
+        trans_conv_2 = tf.nn.conv2d_transpose(trans_conv_1, self.trans_conv_w_2, output_shape=[self.batch_size, 28, 28, 1], strides=[1, 2, 2, 1], padding='VALID')+self.trans_conv_b_2
 
-		# Generated output
-		self.output = tf.nn.sigmoid(trans_conv_2)
+        # Generated output
+        self.output = tf.nn.sigmoid(trans_conv_2)
 
     def getReconstructionLoss(self):
         # obtain the negative log likelihood for bernoulli distribution
