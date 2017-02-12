edf2e9fe65d1668000b0af5c1aed6cb95dbad43e
R S Nikhil Krishna <rsnk96@gmail.com>
2017-02-12 16:26:04 +0530

    Updated dataAugment for MNIST

--- dataAugment.py
+++ dataAugment.py
@@ -1,28 +1,27 @@
 from skimage import transform
 import numpy as np
 from matplotlib import pyplot as plt
-import glob
 
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("./data/", one_hot=True)
 
-img_list = glob.glob("test_images/*.jpg")
-images = [rgb2gray(plt.imread(ele)) for ele in img_list]
-print(len(images))
+IMAGE_DIM = 28
 
-shift_y, shift_x = (np.array(images[0].shape)-1) / 2.   #Assumption: All images of same size
+shift_y, shift_x = (IMAGE_DIM-1) / 2.,(IMAGE_DIM-1) / 2.   #Assumption: All images of same size
 tf_rotate = transform.SimilarityTransform(rotation=np.deg2rad(60))
 tf_shift = transform.SimilarityTransform(translation=[-shift_x, -shift_y])
 tf_shift_inv = transform.SimilarityTransform(translation=[shift_x, shift_y])
 
-for image in images:
-    image_rotated = transform.warp(image, (tf_shift + (tf_rotate + tf_shift_inv)).inverse, order = 3)
+batch_size = 2
 
+batch = mnist.train.next_batch(batch_size)
+reshaped_input = np.reshape(batch[0], (-1, IMAGE_DIM, IMAGE_DIM, 1))
+for i in  range(batch_size):
+    image_rotated = transform.warp(reshaped_input[i,:,:,0], (tf_shift + (tf_rotate + tf_shift_inv)).inverse, order = 3)    
     plt.subplot(121)
-    plt.imshow(image)
+    plt.imshow(reshaped_input[i,:,:,0])
     plt.subplot(122)
     plt.imshow(image_rotated)
     plt.show()
 
-    print("original image maximum at: ", np.unravel_index(np.argmax(image), image.shape))
-    print("rotated image maximum at : ", np.unravel_index(np.argmax(image_rotated), image_rotated.shape))
