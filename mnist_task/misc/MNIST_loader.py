6497a520cc60e3ae6cb7f6d87d7fc5943fe6c26e
Santhosh <santhoshkumar.25dec@gmail.com>
2017-02-15 05:26:58 +0530

    added code to rain on augmented MNIST

---
+++ mnist_task/misc/MNIST_loader.py
@@ -0,0 +1,32 @@
+import h5py
+import numpy as np
+
+class MNIST_loader(object):
+
+    def __init__(self, params):
+		self.h5_file = h5py.File(params['h5_file'], 'r')
+		self.batch_size = params.get('batch_size', 20)
+		self.total_size = self.h5_file['augmented'].shape[0]
+		self.shuffle = params.get('shuffle',  0)
+		self.data = np.array(self.h5_file['augmented'])
+		if self.shuffle == 1:
+			self.data = np.random.shuffle(self.data)
+
+		self.shapes = self.data.shape[1:]
+		self.shapes = (self.batch_size, self.shapes[0], self.shapes[1], self.shapes[2])
+		self.iterator = 0
+
+    def next_batch(self):
+        data_next = np.zeros(self.shapes)
+        if self.iterator + self.batch_size - 1 < self.total_size:
+            data_next[:, :, :, :] = np.array(self.data[self.iterator:(self.iterator+self.batch_size), :, :, :])
+            self.iterator += self.batch_size
+        else:
+            num_remains = self.batch_size - (self.total_size - self.iterator+1)
+            data_next[:(self.total_size-self.iterator+1), :, :, :] = np.array(self.data[self.iterator:self.total_size])
+            # Wrap around
+            data_next[(self.total_size-self.iterator+1):, :, :, :] = np.array(self.data[0:num_remains, :, :, :])
+            self.iterator = num_remains
+        
+        return data_next
+        
--- mnist_task/train_augmented_mnist.py
+++ mnist_task/train_augmented_mnist.py
@@ -4,38 +4,44 @@ import numpy as np
 import os
 import scipy.misc
 import argparse
+import h5py
+import sys
+sys.path.append("misc/")
+from MNIST_loader import MNIST_loader
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--vae_model', default='mlp', help='[ mlp | cnn ]')
 parser.add_argument('--save_dir', default='generated')
+parser.add_argument('--input_h5', default='data/augmented_mnist.h5')
 
 commandline_params = vars(parser.parse_args())
 model_choice = commandline_params['vae_model']
 if model_choice == 'mlp':
     import betavae_mlp as vae
 elif model_choice == 'cnn':
-    import betavae_cnn as vae
+    import betavae_cnn_aug as vae
 else:
     raise ValueError('Wrong model choice for VAE!')
 
-# Import MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("./data/", one_hot=True)
+
 sess = tf.InteractiveSession()
 
 tr_iters = 200000
 
 params = {}
 params['z_size'] = 20
-params['beta'] = 1
+params['beta'] = 4
 params['batch_size'] = 20
 if model_choice == 'mlp':
-    params['X_size'] = 784
+    params['X_size'] = 1600
     params['hidden_enc_1_size'] = 500
     params['hidden_enc_2_size'] = 200
     params['z_size'] = 20
     params['hidden_gen_1_size'] = 200
     params['hidden_gen_2_size'] = 500
+
+# Import MNIST data
+loader = MNIST_loader({'h5_file':commandline_params['input_h5'], 'batch_size':params['batch_size'], 'shufflle': 1})
 
 params_generated = params
 
@@ -54,25 +60,25 @@ saver = tf.train.Saver()
 
 for i in range(tr_iters):
     
-    batch = mnist.train.next_batch(params['batch_size'])
+    batch = loader.next_batch()
     if model_choice == 'cnn':
-        inputReshaped = np.reshape(batch[0], (-1, 28, 28, 1))
+        _, loss_val = sess.run([train_step, VAE.total_loss], \
+                               feed_dict = {VAE.X_placeholder: batch})
+    else:
+        inputReshaped = np.reshape(batch, (-1, 40*40))
         _, loss_val = sess.run([train_step, VAE.total_loss], \
                                feed_dict = {VAE.X_placeholder: inputReshaped})
-    else:
-        _, loss_val = sess.run([train_step, VAE.total_loss], \
-                               feed_dict = {VAE.X_placeholder: batch[0]})
 
     if i % 1000 == 0:
         print('Iteration %.4d  Train Loss: %6.3f'%(i+1, loss_val))
 
-    if (i+1) % 50000 == 0 or i == 0:
+    if (i+1) % 250000 == 0 or i == 0:
         generated = VAE.generateSample(sess, n_samples=params['batch_size'])
-        os.system('mkdir -p %s/iter_%.6d'%(params['save_dir'], i+1))
+        os.system('mkdir -p %s/iter_%.6d'%(commandline_params['save_dir'], i+1))
         for im in range(params['batch_size']):
             reshaped_image = generated[im]
-            reshaped_image = reshaped_image.reshape(28, 28)
-            scipy.misc.toimage(reshaped_image, cmin=0.0, cmax=1.0).save('%s/iter_%.6d/img%.3d.png'%(params['save_dir'],i+1,im))
+            reshaped_image = reshaped_image.reshape(40, 40)
+            scipy.misc.toimage(reshaped_image, cmin=0.0, cmax=1.0).save('%s/iter_%.6d/img%.3d.png'%(commandline_params['save_dir'],i+1,im))
 
-        save_path = saver.save(sess, '%s/iter_%.6d/checkpoint.ckpt'%(params['save_dir'],i+1))
+        save_path = saver.save(sess, '%s/iter_%.6d/checkpoint.ckpt'%(commandline_params['save_dir'],i+1))
         print('Saved model to %s'%(save_path))
