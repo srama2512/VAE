5db204531016ad63fde95b56e0214f3503f2d087
R S Nikhil Krishna <rsnk96@gmail.com>
2017-02-18 08:50:48 +0530

    Added getMeanVariance() to betavae_mlp

--- mnist_task/betavae_mlp.py
+++ mnist_task/betavae_mlp.py
@@ -6,7 +6,7 @@ def getBias(shape):
     return tf.Variable(tf.constant(0, shape=shape, dtype=tf.float32), dtype=tf.float32)
 
 class vae(object):
-	
+    
     def __init__(self, params):
         # params consists of:
         # (1) hidden_enc_1_size: 1st hidden layer size of encoder
@@ -58,7 +58,7 @@ class vae(object):
         '''
         Use the most likely z for given x to generate a sample instead of
         sampling from Q(z|x)
-		'''
+        '''
         return self.decode(sess,self.encode_ML(sess,x))
     
     
@@ -105,12 +105,11 @@ class vae(object):
         self.log_Sigma_X_diag = tf.matmul(hidden_2, self.Sigma_weight) + self.Sigma_bias
 
     def getLatentSampler(self):
-
         self.eps = tf.random_normal([self.batch_size, self.z_size], 0, 1, dtype=tf.float32)
-		if tf.__version__ == '0.10.0':
-			self.z_sample = tf.mul(tf.sqrt(tf.exp(self.log_Sigma_X_diag)), self.eps) + self.mu_X
-		else:
-			self.z_sample = tf.multiply(tf.sqrt(tf.exp(self.log_Sigma_X_diag)), self.eps) + self.mu_X
+        if tf.__version__ == '0.10.0':
+            self.z_sample = tf.mul(tf.sqrt(tf.exp(self.log_Sigma_X_diag)), self.eps) + self.mu_X
+        else:
+            self.z_sample = tf.multiply(tf.sqrt(tf.exp(self.log_Sigma_X_diag)), self.eps) + self.mu_X
 
     def getGenerator(self):
         # Hidden layer 1
@@ -144,4 +143,7 @@ class vae(object):
         '''
         self.kl_loss = self.beta*tf.reduce_sum(tf.exp(self.log_Sigma_X_diag) + \
                                                                  tf.square(self.mu_X) - 1 - self.log_Sigma_X_diag, 1)
-
+    
+    def getMeanVariance(self,sess,x) :
+        mu, log_sigma = sess.run([self.mu_X, self.log_Sigma_X_diag], feed_dict={self.X_placeholder: x})
+        return mu, np.exp(log_sigma)
